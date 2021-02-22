abe777d8c5fc8c3cc46348899a228add3ccc76b0
thisnameisprivate <2632798785@qq.com>
2021-02-22 23:57:54 +0800

    更新了签到页面每月天数

--- pages/game/game.js
+++ pages/game/game.js
@@ -5,62 +5,36 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        weekday: ['日', '一', '二', '三', '四', '五', '六'],
+        monthNum: [],
+        todayTime: null,
+        //今天的日期
+        date: new Date().getDate()
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    // 获取本月日期
+    onLoad: function () {
+        // 实例化Date
+        let date = new Date();
+        // 获取当前月份
+        let currMonth = date.getMonth()
+        // 获取实际月份(因为getMonth获取的月份会比当前月份少1)
+        date.setMonth(currMonth + 1);
+        // 重置日期为0, 否则只显示当前日期
+        date.setDate(0);
+        this.marageDay(date.getDate());
+        let getMonth = new Date().getMonth() + 1;
+        this.setData({
+            monthNum: this.data.monthNum,
+            todayTime: new Date().getFullYear() + "年" + getMonth  + "月" + new Date().getDate() + "日"
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    marageDay: function (arrayLength) {
+        let date = new Date();
+        for (var i = 1; i <= arrayLength; i ++) {
+            let currDay = date.getFullYear() + "/" + date.getMonth() + 1 + "/" + i;
+            this.data.monthNum.push({
+                'date': i, 'day': this.data.weekday[new Date(currDay).getDay()]
+            })
+        }
     }
 })
