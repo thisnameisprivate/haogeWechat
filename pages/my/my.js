05aca6138d381fdc78d3c668d06979d2999be3af
thisnameisprivate <2632798785@qq.com>
2021-02-27 15:43:39 +0800

    更新我的信息页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,62 +5,56 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        Mapres: "未找到当前位置信息",
+        mapImg:     ['../../images/titleImage/map.png'],
+        searchImg:  ['../../images/titleImage/search.png'],
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        // 用户信息
+        userInfo: null,
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        var that = this;
+        // 从storage中获取当前定位信息
+        wx.getStorage({
+            key: "ipaddress",
+            success: res => {
+                that.setData({
+                    Mapres: res.data
+                })
+            }
+        });
+        wx.getUserInfo({
+            success: res => {
+                console.log(res)    //获取的用户信息还有很多，都在res中，看打印结果
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            },
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    // 改变当前位置, 并将新的结果储存到storage
+    getgps: function () {
+        var that = this;
+        wx.chooseLocation({
+            success: function (res) {
+                that.setData({
+                    Mapres: res.name
+                });
+                wx.setStorage({
+                    key: "ipaddress",
+                    data: that.data.Mapres
+                })
+            }
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    bindGetUserInfo (e) {
+        this.setData({
+            userInfo: e.detail.userInfo
+        })
     }
 })
