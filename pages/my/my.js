94c33f0e5121543dfec1d413ac9e9246529a00af
thisnameisprivate <2632798785@qq.com>
2021-03-02 21:15:28 +0800

    修复首页今日热搜文章title过长导致左边图片变形的bug

--- pages/my/my.js
+++ pages/my/my.js
@@ -29,7 +29,6 @@ Page({
         });
         wx.getUserInfo({
             success: res => {
-                console.log(res)    //获取的用户信息还有很多，都在res中，看打印结果
                 this.setData({
                     userInfo: res.userInfo,
                     hasUserInfo: true
